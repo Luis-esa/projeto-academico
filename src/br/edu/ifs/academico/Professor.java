bcb502fe50a847f0a84a396921cb03d81e2d01f3
Luis Eduardo <168037137+Luis-esa@users.noreply.github.com>
2024-11-24 10:56:48 -0300

    Create Professor.java
    Estendendo da classe Pessoa, criando atributos e encapsulando-os.

---
+++ src/br/edu/ifs/academico/Professor.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.academico;
+
+public class Professor extends Pessoa {
+    private String formacao;
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+}

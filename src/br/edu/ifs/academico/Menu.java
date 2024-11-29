76a54222f55fe5ccd13eb78d8453e82384416214
Luis Eduardo <168037137+Luis-esa@users.noreply.github.com>
2024-11-29 20:47:18 -0300

    Criando menu de opçôes que será lida pelo usuário

---
+++ src/br/edu/ifs/academico/Menu.java
@@ -0,0 +1,19 @@
+package br.edu.ifs.academico;
+
+public class Menu {
+    public void imprimir() {
+        System.out.println("-----------MENU----------");
+        System.out.println("selecione uma opcao: "
+                + "\n1 - cadastrar aluno"
+                + "\n2 - listar alunos"
+                + "\n3 - cadastrar professor"
+                + "\n4 - listar professores"
+                + "\n5 - cadastrar pedagogo"
+                + "\n6 - listar pedagogo"
+                + "\n7 - cadastrar psicologo"
+                + "\n8 - listar psicologo"
+                + "\n9 - cadastrar tecnico"
+                + "\n10- listar tecnico"
+                + "\n0 - SAIR");
+    }
+}
